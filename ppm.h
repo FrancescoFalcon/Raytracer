8eee9ea94bd0ec4536f998b99d026070fdb274e4
FrancescoFalcon <francesco.falcon777@gmail.com>
2025-08-24 20:58:27 +0200

    refactor: remove redundant comments and clean up header files

--- ppm.h
+++ ppm.h
@@ -11,20 +11,3 @@ Francesco Falcon Sm3201408
 int ppm_write_mmap(const char* path, int width, int height, const rgb24* data);
 
 #endif // PPM_H
-/*
-Francesco Falcon Sm3201408
- */
-#ifndef PPM_H
-#define PPM_H
-
-#include <stdint.h>
-#include <stddef.h>
-#include "scene.h"
-
-// Scrive immagine PPM (P6) usando mmap in modo portabile (POSIX e Windows).
-// Formato P6: header ASCII "P6\n<width> <height>\n255\n" seguito da width*height pixel
-// in ordine riga-per-riga dall'alto verso il basso, ciascuno come 3 byte RGB.
-// Ritorna 0 se ok, !=0 su errore.
-int ppm_write_mmap(const char* path, int width, int height, const rgb24* data);
-
-#endif // PPM_H
